663dcf97da870ed648b19f71750b1bef46ded2f1
Laith <Pikachu.jo02@gmail.com>
2024-03-13 15:32:02 +0300

    13/3/2024

--- Elibrary_Web/Areas/Customer/Controllers/CartController.cs
+++ Elibrary_Web/Areas/Customer/Controllers/CartController.cs
@@ -50,10 +50,12 @@ namespace Elibrary_Web.Areas.Customer.Controllers
         }
         public IActionResult Minus(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
-            if (cartFromDb.Count == 1)
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId , tracked:true);
+            if (cartFromDb.Count <= 1)
             {
                 _unitOfWork.ShoppingCart.Remove(cartFromDb);
+                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
+                    .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
             }
             else
             {
@@ -67,7 +69,6 @@ namespace Elibrary_Web.Areas.Customer.Controllers
         public IActionResult Remove(int cartId)
         {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId , tracked:true);
-
             HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(U => U.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1 );
             _unitOfWork.ShoppingCart.Remove(cartFromDb);
             _unitOfWork.Save();
