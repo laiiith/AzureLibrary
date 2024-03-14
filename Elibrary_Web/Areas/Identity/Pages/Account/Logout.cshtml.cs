6a89432ba563b8789b52f55c732020811cb584fd
Laith <Pikachu.jo02@gmail.com>
2024-03-14 12:33:23 +0300

    Fix Host

--- Elibrary_Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Elibrary_Web/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -32,9 +32,7 @@ namespace Elibrary.Areas.Identity.Pages.Account
                 return LocalRedirect(returnUrl);
             }
             else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
+            { 
                 return RedirectToPage();
             }
         }
