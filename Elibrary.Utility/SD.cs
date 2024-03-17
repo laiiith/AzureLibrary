8896928a0d7ebe4672808531d92c1b267c915132
Laith <Pikachu.jo02@gmail.com>
2024-03-17 13:35:49 +0300

    !

--- Elibrary.Utility/SD.cs
+++ Elibrary.Utility/SD.cs
@@ -9,6 +9,7 @@ namespace Elibrary.Utility
     public static class SD
     {
         public const string Role_Customer = "Customer";
+        public const string Test = "Test";
         public const string Role_Company = "Company";
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
